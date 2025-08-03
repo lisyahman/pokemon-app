786f70181807593a0ecc785c01e09c04ed77e9af
Allisyah <allisyah@unisel.edu.my>
2025-08-03 11:29:05 +0800

    Fix vite base config for Netlify

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // ❗ Ini penting untuk Netlify deploy dengan betul
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
